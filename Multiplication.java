1b4ab4db7d42e3702c07be85531926ef3ece006d
Sriharithaa <114979682+Sriharithaa@users.noreply.github.com>
2022-10-17 13:17:37 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Multiplication {
+
+	public static void main(String[] args) {
+		
+		System.out.println("multiplication tables from 2 to 10 are:");
+	
+	
+	for(int l=2;l<=10;l++) {
+		
+		for(int j=1;j<=10;j++) {
+			
+		}
+		System.out.println(l+"x"+j+"="+i*j);
+	}
+		
+		
+
+	}
+}
